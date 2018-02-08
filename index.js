fc06113ab3fa77e4950fe057045aaeff6c5792a8
Aaron Shafovaloff <aaronshaf@gmail.com>
2018-02-08 13:04:12 -0700

    use older js
    in some build environments (such as create-react-app's), es6 code cannot be minified

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const map = f => source => (start, sink) => {
-  if (start !== 0) return;
-  source(0, (t, d) => {
-    sink(t, t === 1 ? f(d) : d)
-  });
+var map = function map(f) {
+  return function(source) {
+    return function(start, sink) {
+      if (start !== 0) return;
+      source(0, function(t, d) {
+        sink(t, t === 1 ? f(d) : d);
+      });
+    };
+  };
 };
 
 module.exports = map;
